41c8636806784bdec3b864704c30db73d9ecb3b2
Oleksii <oleksii.vrublevskyi@gmail.com>
2023-10-04 17:23:06 +0300

    Add print

--- gtrans.py
+++ gtrans.py
@@ -3,7 +3,7 @@ import mypackage.module1 as md1
 text = "Hello world"
 dest = 'uk'
 lang = 'en'
-
+#print
 print(md1.TransLate(text=text, dest=dest))
 print(md1.LangDetect(text=text, set='all'))
 print(md1.CodeLang(lang=lang))
